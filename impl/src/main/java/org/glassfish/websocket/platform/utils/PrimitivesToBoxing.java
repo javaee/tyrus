850b2a6047c87ff69cdbb4896230a820ddc9794e
stepan.kopriva <stepan.kopriva@aa68c1a9-5198-4982-ce7d-eee0ef5d8746>
2012-09-11 08:38:34 +0000

    Ported to API v 003, echo sample working.
    git-svn-id: https://svn.java.net/svn/tyrus~source-code-repository/trunk@10 aa68c1a9-5198-4982-ce7d-eee0ef5d8746

---
+++ impl/src/main/java/org/glassfish/websocket/platform/utils/PrimitivesToBoxing.java
@@ -0,0 +1,41 @@
+package org.glassfish.websocket.platform.utils;
+
+import java.util.HashMap;
+
+/**
+ * For the given primitive type returns finds it's boxing alternative.
+ *
+ * @author Stepan Kopriva (stepan.kopriva at oracle.com)
+ */
+public class PrimitivesToBoxing{
+
+    private static HashMap<Class<?>, Class<?>> conversionMap = null;
+
+    /**
+     * Gets the Boxing class for the primitive type.
+     *
+     * @param input primitive type
+     * @return boxing class if input is primitive type, input otherwise
+     */
+    public static Class<?> getBoxing(Class<?> input){
+        if(!input.isPrimitive()){
+            return input;
+        }
+        if(conversionMap == null){
+            initConversionMap();
+        }
+        return conversionMap.containsKey(input) ? conversionMap.get(input) : input;
+    }
+
+    private static void initConversionMap(){
+        conversionMap = new HashMap<Class<?>, Class<?>>();
+        conversionMap.put(int.class, Integer.class);
+        conversionMap.put(short.class, Short.class);
+        conversionMap.put(long.class, Long.class);
+        conversionMap.put(double.class, Double.class);
+        conversionMap.put(float.class, Float.class);
+        conversionMap.put(boolean.class, Boolean.class);
+        conversionMap.put(byte.class, Byte.class);
+        conversionMap.put(char.class, Character.class);
+    }
+}
