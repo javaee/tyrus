211673d71f52be21054409c0c94ccf678b1e6915
dannycoward <dannycoward@aa68c1a9-5198-4982-ce7d-eee0ef5d8746>
2012-10-18 20:38:19 +0000

    fixed wrong import....did a org.glassfish.tyrus.platform.configuration.* package not get checked in ?
    git-svn-id: https://svn.java.net/svn/tyrus~source-code-repository/trunk@113 aa68c1a9-5198-4982-ce7d-eee0ef5d8746

--- client/src/test/java/org/glassfish/tyrus/client/ClientManagerTest.java
+++ client/src/test/java/org/glassfish/tyrus/client/ClientManagerTest.java
@@ -1,6 +1,6 @@
 package org.glassfish.tyrus.client;
 
-import org.glassfish.tyrus.platform.configuration.DefaultClientEndpointConfiguration;
+import org.glassfish.tyrus.platform.DefaultClientEndpointConfiguration;
 import org.glassfish.tyrus.platform.main.Server;
 import org.junit.Assert;
 import org.junit.Test;
