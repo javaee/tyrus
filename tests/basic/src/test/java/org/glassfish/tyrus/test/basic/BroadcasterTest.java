4ad85e2620ec36519dfd4cd6501ea4e7412f9972
dannycoward <dannycoward@aa68c1a9-5198-4982-ce7d-eee0ef5d8746>
2012-10-18 20:42:10 +0000

    fixed wrong import....did a org.glassfish.tyrus.platform.configuration.* package not get checked in ?
    git-svn-id: https://svn.java.net/svn/tyrus~source-code-repository/trunk@114 aa68c1a9-5198-4982-ce7d-eee0ef5d8746

--- tests/basic/src/test/java/org/glassfish/tyrus/test/basic/BroadcasterTest.java
+++ tests/basic/src/test/java/org/glassfish/tyrus/test/basic/BroadcasterTest.java
@@ -41,7 +41,7 @@
 package org.glassfish.tyrus.test.basic;
 
 import org.glassfish.tyrus.client.ClientManager;
-import org.glassfish.tyrus.platform.configuration.DefaultClientEndpointConfiguration;
+import org.glassfish.tyrus.platform.DefaultClientEndpointConfiguration;
 import org.glassfish.tyrus.platform.main.Server;
 import org.glassfish.tyrus.test.basic.bean.BroadcasterTestBean;
 import org.junit.Ignore;
