cb78caaa7da344348ecd0808c4a7ae009d890f13
dannycoward <dannycoward@aa68c1a9-5198-4982-ce7d-eee0ef5d8746>
2012-10-20 23:39:50 +0000

    Tyrus-2 Code cleanup, statelock
    git-svn-id: https://svn.java.net/svn/tyrus~source-code-repository/trunk@118 aa68c1a9-5198-4982-ce7d-eee0ef5d8746

--- impl/src/main/java/org/glassfish/tyrus/platform/AsyncTextToCharStreamAdapter.java
+++ impl/src/main/java/org/glassfish/tyrus/platform/AsyncTextToCharStreamAdapter.java
@@ -1,37 +1,84 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011 - 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
  */
 package org.glassfish.tyrus.platform;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.net.websocket.MessageHandler;
-import java.io.Reader;
-import java.util.*;
 
 /**
- * 
+ * A simple adapter that acts as a listens for text message fragments on one thread and offers
+ * a (blocking) Reader that can be read on another thread, buffering content along the way.
  * @author dannycoward
  */
-public class AsyncTextToCharStreamAdapter implements BufferedStringSource, MessageHandler.AsyncText {
+class AsyncTextToCharStreamAdapter implements BufferedStringSource, MessageHandler.AsyncText {
+    /* The text message pieces currently being buffered. */
     private List<String> bufferedFragments = new ArrayList<String>();
+    /* Has this adapter received the last in a sequence of fragments. */
     private boolean receivedLast = false;
+    /* The reader implementation this adapter will offer. */
     private BufferedStringSourceReader reader = null;
-    private boolean readerThreadStopped = false;
+    /* Configurable buffer size. */
     private static long MAX_BUFFER_SIZE = 8 * 1024;
+    /* The MessageHandler that will be invoked when a new message starts. */
     private MessageHandler.CharacterStream mh;
+    /* Statelock to mediate between the notification thread for message fragments and the
+     * thread reading the Reader data.
+     */
+    private Object stateLock;
     
     public AsyncTextToCharStreamAdapter(MessageHandler.CharacterStream mh) {
         this.mh = mh;
+        this.stateLock = new Object();
     }
     
     private void blockOnReaderThread() {
-        readerThreadStopped = true;
-        try {
-            while (readerThreadStopped) {
-                Thread.sleep(100);
+        synchronized (stateLock) {
+            try {
+                this.stateLock.wait();
+            } catch (InterruptedException e) {
+                // thread unblocked
             }
-        } catch (Exception e) {}
+        }
     }
+
     
     public char[] getNextChars(int numberOfChars) {
 
@@ -39,7 +86,7 @@ public class AsyncTextToCharStreamAdapter implements BufferedStringSource, Messa
             if (receivedLast) {
                 this.reader = null;
                 return null;
-            } else { // there's more to come...
+            } else { // there's more to come...so wait here...
               blockOnReaderThread();
             }
         }
@@ -76,8 +123,15 @@ public class AsyncTextToCharStreamAdapter implements BufferedStringSource, Messa
         this.receivedLast = last;
         this.checkForBufferOverflow(part);
         bufferedFragments.add(part);
-        this.readerThreadStopped = false;
         
+        synchronized (stateLock) {
+            try {
+                this.stateLock.notifyAll();
+            } catch (Exception e) {
+                
+            }
+        }
+         
         if (this.reader == null) {
             this.reader = new BufferedStringSourceReader(this);
             Thread t = new Thread() {
