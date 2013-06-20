aa999b91e39e95798e7fa661d65ad99fe8d36032
pavel_bucek <pavel_bucek@aa68c1a9-5198-4982-ce7d-eee0ef5d8746>
2013-06-20 12:22:42 +0000

    Introducing Tyrus Extension - CLI client - minor fixes in client proxy support (validation, error reporting)
    git-svn-id: https://svn.java.net/svn/tyrus~source-code-repository/trunk@684 aa68c1a9-5198-4982-ce7d-eee0ef5d8746

--- ext/client-cli/src/main/java/org/glassfish/tyrus/ext/client/cli/ClientCli.java
+++ ext/client-cli/src/main/java/org/glassfish/tyrus/ext/client/cli/ClientCli.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.glassfish.tyrus.extensions.client.cli;
+package org.glassfish.tyrus.ext.client.cli;
 
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
