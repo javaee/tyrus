423bb7e61028fdf9b4fd7833df0d5502741b4140
Petr Janouch <petr.janouch@oracle.com>
2014-08-28 10:54:10 +0200

    TYRUS-285: parallel broadcast test added

---
+++ tests/e2e/standard-config/src/test/java/org/glassfish/tyrus/test/standard_config/ParallelBroadcastTest.java
@@ -0,0 +1,149 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.tyrus.test.standard_config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.glassfish.tyrus.client.ClientManager;
+import org.glassfish.tyrus.client.ClientProperties;
+import org.glassfish.tyrus.server.Server;
+import org.glassfish.tyrus.test.tools.TestContainer;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Petr Janouch (petr.janouch at oracle.com)
+ */
+public class ParallelBroadcastTest extends TestContainer {
+
+    /**
+     * Number of created sessions.
+     */
+    public static final int SESSIONS_COUNT = 1000;
+    /**
+     * Number of threads used for executing the broadcast.
+     */
+    public static final int THREAD_COUNT = 7;
+
+    @Test
+    public void testParallelBroadcastOnServer() {
+        Server server = null;
+        AtomicInteger messageCounter = new AtomicInteger(0);
+        CountDownLatch messageLatch = new CountDownLatch(SESSIONS_COUNT);
+        try {
+            server = startServer(BroadcastServerEndpoint.class);
+            ClientManager client = ClientManager.createClient();
+            client.getProperties().put(ClientProperties.SHARED_CONTAINER, true);
+
+            for (int i = 0; i < SESSIONS_COUNT - 1; i++) {
+                client.connectToServer(new AnnotatedClientEndpoint(messageLatch, messageCounter), getURI(BroadcastServerEndpoint.class));
+            }
+
+            Session session = client.connectToServer(new AnnotatedClientEndpoint(messageLatch, messageCounter), getURI(BroadcastServerEndpoint.class));
+            session.getBasicRemote().sendText("Broadcast request");
+
+            assertTrue(messageLatch.await(30, TimeUnit.SECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        } finally {
+            System.out.println("Received messages: " + messageCounter + "/" + SESSIONS_COUNT);
+            stopServer(server);
+        }
+    }
+
+    @ClientEndpoint
+    public static class AnnotatedClientEndpoint {
+
+        private final CountDownLatch messageLatch;
+        private final AtomicInteger messageCounter;
+
+        public AnnotatedClientEndpoint(CountDownLatch messageLatch, AtomicInteger messageCounter) {
+            this.messageLatch = messageLatch;
+            this.messageCounter = messageCounter;
+        }
+
+        @OnMessage
+        public void onMessage(String message) {
+            messageCounter.incrementAndGet();
+            messageLatch.countDown();
+        }
+    }
+
+    @ServerEndpoint("/parallelBroadcastEndpoint")
+    public static class BroadcastServerEndpoint {
+
+        @OnMessage
+        public void onMessage(final Session session, String message) {
+            final List<Session> openSessions = new ArrayList<Session>(session.getOpenSessions());
+            ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+
+            for (int i = 0; i < THREAD_COUNT; i++) {
+                final int partitionId = i;
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int lowerBound = (SESSIONS_COUNT + THREAD_COUNT - 1) / THREAD_COUNT * partitionId;
+                        int upperBound = Math.min((SESSIONS_COUNT + THREAD_COUNT - 1) / THREAD_COUNT * (partitionId + 1), SESSIONS_COUNT);
+                        System.out.println(Thread.currentThread().getName() + " <" + lowerBound + ", " + upperBound + ")");
+
+                        for (int j = lowerBound; j < upperBound; j++) {
+                            openSessions.get(j).getAsyncRemote().sendText("Hi from " + partitionId);
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
