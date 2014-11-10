6782d8b8ad23a889779803d4656d5b2a36ea512d
Pavel Bucek <pavel.bucek@oracle.com>
2014-11-10 15:55:07 +0100

    Mac OS X 10.10: yosemite does not correctly apply ::1 as non-proxiable host.

--- tests/e2e/non-deployable/src/test/java/org/glassfish/tyrus/test/e2e/non_deployable/IPv6EchoTest.java
+++ tests/e2e/non-deployable/src/test/java/org/glassfish/tyrus/test/e2e/non_deployable/IPv6EchoTest.java
@@ -57,6 +57,7 @@ import javax.websocket.server.ServerEndpoint;
 import org.glassfish.tyrus.server.Server;
 import org.glassfish.tyrus.test.tools.TestContainer;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
@@ -74,6 +75,7 @@ public class IPv6EchoTest extends TestContainer {
     }
 
     @Test
+    @Ignore("jdk@yosemite bug - does not correctly apply ::1 as non-proxiable host.")
     public void testIPv6() throws DeploymentException {
         Server server = startServer(EchoEndpoint.class);
 
